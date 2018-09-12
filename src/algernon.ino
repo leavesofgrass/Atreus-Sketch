2e4cf1f8e6a64e81746d1d89032d3dc3c914828e
Gergely Nagy <kaleidoscope@gergo.csillger.hu>
2018-09-12 09:00:50 +0200

    Initial import
    Signed-off-by: Gergely Nagy <kaleidoscope@gergo.csillger.hu>

---
+++ src/algernon.ino
@@ -0,0 +1,74 @@
+/* -*- mode: c++ -*-
+ * Atreus-Sketch -- algernon's Atreus Sketch
+ * Copyright (C) 2018  Gergely Nagy
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "00-config.h"
+
+#include "Kaleidoscope.h"
+#include "Kaleidoscope-Macros.h"
+#if WITH_CYCLETIMEREPORT
+#include "Kaleidoscope-CycleTimeReport.h"
+#endif
+
+enum {
+  M_RESET
+};
+
+#include "keymap.h"
+
+KALEIDOSCOPE_INIT_PLUGINS(
+    Macros
+#if WITH_CYCLETIMEREPORT
+    ,CycleTimeReport
+#endif
+);
+
+
+#if WITH_CYCLETIMEREPORT
+/* CycleTimeReport */
+
+void cycleTimeReport(void) {
+  Serial.print(F("# average loop time: "));
+  Serial.println(CycleTimeReport.average_loop_time);
+}
+#endif
+
+/** Macros **/
+
+const macro_t *macroAction(uint8_t macroIndex, uint8_t keyState) {
+  if (!keyToggledOn(keyState))
+    return MACRO_NONE;
+
+  switch (macroIndex) {
+    case M_RESET:
+      Atreus.resetDevice();
+      break;
+  }
+
+  return MACRO_NONE;
+}
+
+/** MAIN **/
+
+void setup() {
+  Serial.begin(9600);
+  Kaleidoscope.setup();
+}
+
+void loop() {
+  Kaleidoscope.loop();
+}
