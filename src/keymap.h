601473919d10c1872c4e8857d05c24fae240697a
Gergely Nagy <kaleidoscope@gergo.csillger.hu>
2018-10-11 17:05:42 +0200

    Major rearrangement of the TESO layers
    Fixes #5.
    
    Signed-off-by: Gergely Nagy <kaleidoscope@gergo.csillger.hu>

--- src/keymap.h
+++ src/keymap.h
@@ -95,28 +95,28 @@ KEYMAPS(
 
   [TESO] = KEYMAP_STACKED
   (
-       Key_1   ,Key_2 ,Key_3       ,Key_4           ,Key_5
-      ,Key_Q   ,Key_W ,Key_E       ,Key_R           ,Key_Esc
-      ,Key_A   ,Key_S ,Key_D       ,Key_F           ,Key_Backtick
-      ,MO(AUX) ,Key_H ,Key_LeftAlt ,Key_LeftControl ,Key_Space ,Key_LeftShift
+       Key_1        ,Key_2 ,Key_3           ,Key_4       ,Key_5
+      ,Key_Backtick ,Key_W ,Key_E           ,Key_R       ,Key_Q
+      ,Key_A        ,Key_S ,Key_D           ,Key_F       ,Key_Esc
+      ,OSL(AUX)     ,Key_M ,Key_LeftControl ,Key_LeftAlt ,Key_Space ,Key_LeftShift
 
-           ,XXX ,XXX ,XXX ,XXX ,M(M_TYPING)
-           ,XXX ,XXX ,XXX ,XXX ,XXX
-           ,XXX ,XXX ,XXX ,XXX ,XXX
-      ,XXX ,XXX ,XXX ,XXX ,XXX ,XXX
+                ,XXX ,XXX ,XXX ,XXX ,XXX
+                ,XXX ,XXX ,XXX ,XXX ,XXX
+                ,XXX ,XXX ,XXX ,XXX ,XXX
+      ,OSL(AUX) ,XXX ,XXX ,XXX ,XXX ,M(M_TYPING)
   ),
 
   [AUX] = KEYMAP_STACKED
   (
-       XXX   ,Key_P ,Key_M ,Key_U ,Key_J
-      ,XXX   ,Key_1 ,Key_2 ,Key_3 ,Key_I
-      ,Key_0 ,Key_4 ,Key_5 ,Key_6 ,Key_K
-      ,___   ,Key_7 ,Key_8 ,Key_9 ,Key_Space ,Key_Enter
+       XXX       ,Key_P  ,Key_M ,Key_U ,Key_J
+      ,Key_Quote ,XXX    ,Key_E ,Key_R ,Key_I
+      ,Key_F3    ,Key_F5 ,Key_X ,XXX   ,Key_K
+      ,___       ,Key_H  ,XXX   ,XXX   ,Key_Space ,Key_Enter
 
-           ,XXX ,XXX ,XXX ,XXX ,M(M_TYPING)
-           ,XXX ,XXX ,XXX ,XXX ,XXX
-           ,XXX ,XXX ,XXX ,XXX ,XXX
-      ,XXX ,XXX ,XXX ,XXX ,XXX ,XXX
+           ,Key_1 ,Key_2 ,Key_3 ,XXX   ,XXX
+           ,Key_4 ,Key_5 ,Key_6 ,Key_0 ,XXX
+           ,Key_7 ,Key_8 ,Key_9 ,XXX   ,XXX
+      ,___ ,XXX   ,XXX   ,XXX   ,XXX   ,M(M_TYPING)
   ),
 )
 /* *INDENT-ON* */
